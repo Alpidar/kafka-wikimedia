b3e59ad1f4b99b455e9a541c3d6ab8b762574b78
Alpidar <alpidar.ab@gmail.com>
2022-09-12 11:34:55 -0400

    initial producer

---
+++ kafka-producer-wikimedia/src/main/java/alpidar/demos/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,54 @@
+package alpidar.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
+    private final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+    public WikimediaChangeHandler(String topic) {
+        Properties prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.85.46:9092");
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        this.producer = new KafkaProducer<>(prop);
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+
+    }
+
+    @Override
+    public void onClosed() {
+        this.producer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) {
+        log.info(messageEvent.getData());
+        producer.send(new ProducerRecord<>(this.topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) {
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error("Got error during the session" + t);
+    }
+}
--- kafka-producer-wikimedia/src/main/java/alpidar/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/alpidar/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,20 @@
 package alpidar.demos.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
 public class WikimediaChangesProducer {
     public static void main(String[] args) {
-
+        EventHandler handler = new WikimediaChangeHandler("wikimedia.recentchange");
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        try(EventSource eventSource = new EventSource.Builder(handler, URI.create(url)).build()){
+            eventSource.start();
+            TimeUnit.SECONDS.sleep(10);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
