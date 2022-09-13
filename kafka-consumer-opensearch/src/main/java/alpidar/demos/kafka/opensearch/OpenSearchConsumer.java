7d37a96598035effc6b63b3d6c5760cee9a57988
Alpidar <alpidar.ab@gmail.com>
2022-09-12 22:51:44 -0400

    opensearch consumer modification

--- kafka-consumer-opensearch/src/main/java/alpidar/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/alpidar/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,7 +1,154 @@
 package alpidar.demos.kafka.opensearch;
 
+import com.google.gson.JsonParser;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class OpenSearchConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
+
+
     public static void main(String[] args) {
 
+        try (RestHighLevelClient openSearchClient = createOpenSearchClient();
+             KafkaConsumer<String, String> consumer = createKafkaConsumer()
+        ) {
+            // graceful shutdown
+            Thread main = Thread.currentThread();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                log.info("Shutting down the consumer");
+                consumer.wakeup();
+                try {
+                    main.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }));
+
+            // create index
+            boolean indexExist = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+            if (!indexExist) {
+                openSearchClient.indices().create(new CreateIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+                log.info("index has been created");
+            } else {
+                log.info("index already exists");
+            }
+
+            // subscribe to the target topic
+            consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+
+            BulkRequest bulkRequest = new BulkRequest();
+
+            int i = 20;
+            while(i > 0){
+                // consume the data
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(3000));
+                log.info("Received " + consumerRecords.count() + " records");
+
+                for (ConsumerRecord<String, String> record : consumerRecords) {
+                    String id = extractId(record.value());
+                    IndexRequest indexRequest = new IndexRequest("wikimedia").source(record.value(), XContentType.JSON).id(id);
+                    bulkRequest.add(indexRequest);
+                }
+                if(bulkRequest.numberOfActions() > 0){
+
+                    openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+                    TimeUnit.SECONDS.sleep(1);
+
+                    consumer.commitSync();
+                    log.info("Offsets have been committed");
+                }
+                i--;
+
+            }
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public static RestHighLevelClient createOpenSearchClient() {
+        String connString = "http://localhost:9200";
+
+        // we build a URI from the connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        // extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+
+        return restHighLevelClient;
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        String boostrapServers = "192.168.85.46:9092";
+        String groupId = "consumer-opensearch-demo";
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        // create consumer
+        return new KafkaConsumer<>(properties);
+    }
+
+    private static String extractId(String json) {
+        return JsonParser.parseString(json)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
     }
 }
